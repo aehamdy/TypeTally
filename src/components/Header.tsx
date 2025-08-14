63aab3ef5f16bb27960aec45831abedcbbea2665
AhmedEssam <ae.devverse@gmail.com>
2025-08-14 19:20:34 +0300

    Feat: Create Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import Logo from "./Logo";
+
+const Header = () => {
+  return (
+    <header className="w-full py-3 px-4 bg-gray-200 rounded-xl shadow-lg">
+      <Logo />
+    </header>
+  );
+};
+
+export default Header;
