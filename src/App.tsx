3275f0f390052be14f6acc1f9d614aae4af3bab5
AhmedEssam <ae.devverse@gmail.com>
2025-08-14 19:09:45 +0300

    Style: Change and add new styles to adjust positioning and sizes on different screens

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import "./App.css";
+import Header from "./components/Header";
+import MainContent from "./components/MainContent";
 
 function App() {
-  return <></>;
+  return (
+    <main className="h-full py-5 px-2">
+      <Header />
+
+      <div className="w-full h-full">
+        <MainContent />
+      </div>
+    </main>
+  );
 }
 
 export default App;
